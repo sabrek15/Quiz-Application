0b12201028dec4c3151f551b2732323b513673dc
sabrek <reddy.ksb007@gmail.com>
2024-05-04 16:37:14 +0530

    Project updated

--- src/main/java/com/sabrek/quiz/controller/QuizController.java
+++ src/main/java/com/sabrek/quiz/controller/QuizController.java
@@ -1,14 +1,16 @@
 package com.sabrek.quiz.controller;
 
+import com.sabrek.quiz.model.Question;
+import com.sabrek.quiz.model.QuestionWrapper;
+import com.sabrek.quiz.model.Response;
 import com.sabrek.quiz.service.QuizServie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "quiz")
@@ -21,4 +23,15 @@ public class QuizController {
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
         return quizService.createQuiz(category,numQ,title);
     }
+
+    @GetMapping(value = "get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping(value = "submit/{id}")
+    public  ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResults(id, responses);
+    }
+
 }
--- src/main/java/com/sabrek/quiz/service/QuizServie.java
+++ src/main/java/com/sabrek/quiz/service/QuizServie.java
@@ -3,13 +3,19 @@ package com.sabrek.quiz.service;
 import com.sabrek.quiz.dao.QuestionDao;
 import com.sabrek.quiz.dao.QuizDao;
 import com.sabrek.quiz.model.Question;
+import com.sabrek.quiz.model.QuestionWrapper;
 import com.sabrek.quiz.model.Quiz;
+import com.sabrek.quiz.model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class QuizServie {
@@ -26,5 +32,33 @@ public class QuizServie {
         quiz.setQuestions(questions);
         quizDao.save(quiz);
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUsers = new ArrayList<>();
+        for(Question q : questionsFromDB){
+            QuestionWrapper qw = new QuestionWrapper(q.getId(),q.getQuestionTitle(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
+            questionsForUsers.add(qw);
+        }
+
+        return new ResponseEntity<>(questionsForUsers, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> calculateResults(Integer id, List<Response> responses) {
+//        Optional<Quiz> quiz = Optional.of(quizDao.findById(id).get());
+        Quiz quiz = quizDao.findById(id).get();
+        List<Question> questions = quiz.getQuestions();
+        int right = 0;
+        int i = 0;
+        for(Response response : responses){
+            if(Objects.equals(response.getResponse(), questions.get(i).getRightAnswer()))
+                right++;
+
+            i++;
+        }
+
+        return new ResponseEntity<>(right, HttpStatus.OK);
     }
 }
